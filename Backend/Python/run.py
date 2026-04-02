67fb96d97439d69ea8611051c06ee249141a7b57
etemi <etemiomodon@gmail.com>
2026-04-02 10:34:50 +0100

    Make modification to the Docker file

--- Backend/Python/run.py
+++ Backend/Python/run.py
@@ -1,7 +1,6 @@
-
 import os
+from package import app
 
 if __name__ == "__main__":
-    from package import app
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host="0.0.0.0", debug=debug_mode, port=5000)
